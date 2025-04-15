e4d8b7cb07a2459dcb76e6b7d0f8914343b7dcb0
Akhilesh Pareek <122216957+pareekakhilesh123@users.noreply.github.com>
2025-04-15 13:01:22 +0530

    Connect Backend Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,7 +12,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", form);
+      const res = await axios.post("https://jwt-refresh-token-backend.onrender.com/api/auth/login", form);
   
       // ✅ दोनों tokens को store करो
       localStorage.setItem("accessToken", res.data.accessToken);
